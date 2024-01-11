b43ae6da240ffc3efd65cca9c832beb97cd0c57b
Tshiamo <120169525+T-ndlovu@users.noreply.github.com>
2024-01-11 09:39:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from _csv import writer
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, InputRequired
+import csv
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe Name', validators=[DataRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    open = StringField('Opening Time eg: 9am', validators=[DataRequired()])
+    close = StringField('Closing Time eg: 5pm', validators=[DataRequired()])
+    coffee = SelectField('Coffee Rating', choices=['✘', '☕️', '☕️☕️', '☕️☕️☕️', '☕️☕️☕️☕️', '☕️☕️☕️☕️☕️'])
+    wifi = SelectField('Wifi Strength', choices=['✘', '💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪'])
+    power = SelectField('Power Available', choices=['✘', '🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌'])
+    submit = SubmitField('Submit')
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        cafe = form.cafe.data
+        location = form.location.data
+        op = form.open.data
+        cl = form.close.data
+        coffee = form.coffee.data
+        wifi = form.wifi.data
+        power = form.power.data
+        values = cafe, location, op, cl, coffee, wifi, power
+        with open("cafe-data.csv", 'a', encoding='UTF8') as cs:
+            writer_object = writer(cs)
+            writer_object.writerow(values)
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+
+        for row in csv_data:
+            list_of_rows.append(row)
+
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
